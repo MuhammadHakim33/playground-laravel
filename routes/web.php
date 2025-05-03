0327dac180f5fa01d1c50ce2c4aa17bbd1080ae1
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2025-05-03 21:25:48 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/employees', function () {
-    return 'Halo';
-});
+Route::get('/employees', [EmployeeController::class, 'index']);
